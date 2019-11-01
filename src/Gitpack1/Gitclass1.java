828d021c6386958e868dda4d45c6197a7adaae09
anitha8147412450 <56869434+anitha8147412450@users.noreply.github.com>
2019-11-01 17:20:37 +0530

    Update Gitclass1.java

--- src/Gitpack1/Gitclass1.java
+++ src/Gitpack1/Gitclass1.java
@@ -4,7 +4,9 @@ public class Gitclass1 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("ani");// TODO Auto-generated method stub
+		System.out.println("ani");
+		//abc
+		System.out.println("anitha");
 
 	}
 
